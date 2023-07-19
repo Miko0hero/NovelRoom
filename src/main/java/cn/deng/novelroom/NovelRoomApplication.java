1e2bbc6b85c0be6b5000c72bac3826a205b2e8d2
Deng Jinwei <djinwei.foxmail.com>
2023-07-19 21:41:33 +0800

    新建项目

---
+++ src/main/java/cn/deng/novelroom/NovelRoomApplication.java
@@ -0,0 +1,13 @@
+package cn.deng.novelroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/deng/novelroom/NovelRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.deng.novelroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
