d762885e335dc2324aaf6ac1f6a97316b6ec2e61
Deng Jinwei <djinwei.foxmail.com>
2023-07-20 02:17:42 +0800

    将整个项目结构从springboot3.1.1改成2.7.13版本，jdk从17换成1.8，原来的结构很多未知错误

--- src/main/java/cn/deng/novelroom/NovelRoomApplication.java
+++ src/main/java/cn/deng/novelroom/NovelRoomApplication.java
@@ -1,12 +1,19 @@
 package cn.deng.novelroom;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author Deng
+ */
+@Slf4j
 @SpringBootApplication
+@MapperScan(basePackages = "cn.deng.novelroom")
 public class NovelRoomApplication {
 
     public static void main(String[] args) {
+        log.info("是靠的就是到那时可能大佬打瞌睡");
         SpringApplication.run(NovelRoomApplication.class, args);
     }
 
--- src/test/java/cn/deng/novelroom/NovelRoomApplicationTests.java
+++ src/test/java/cn/deng/novelroom/NovelRoomApplicationTests.java
@@ -1,13 +1,15 @@
 package cn.deng.novelroom;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class NovelRoomApplicationTests {
 
     @Test
     void contextLoads() {
+//        log.info("是多少卡机朵拉朵尚可能大胆");
     }
 
 }
